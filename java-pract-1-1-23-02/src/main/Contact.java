731dafff6f66f115b8279055a4736e42da1454ab
Ilona1212 <59233009+Ilona1212@users.noreply.github.com>
2021-02-23 19:59:35 +0200

    Update Contact.java

--- java-pract-1-1-23-02/src/main/Contact.java
+++ java-pract-1-1-23-02/src/main/Contact.java
@@ -1,1 +1,36 @@
-1
+package main;
+
+public class Contact {
+    public int Id;
+    private String firstName;
+    private String lastName;
+    private String address; 
+
+    public String getFirstName() {
+       return firstName;
+   }
+    public void setFirstName(String firstName) {
+       this.firstName = firstName;
+   }
+
+    public String getLastName() {
+       return lastName;
+   }
+    public void setLastName(String lastName) {
+       this.lastName = lastName;
+   }
+
+    public String getAddress() {
+       return address;
+   }
+    public void setAddress(String address) {
+       this.address = address;
+   }
+
+    public Contact(String firstName, String lastName, String address){ 
+    firstName = firstName;
+    lastName = lastName;
+    address = address;
+}
+}
+
