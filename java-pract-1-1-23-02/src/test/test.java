b604f8b66b015627d60d82b3400cf0b32273ee1e
Ilona1212 <59233009+Ilona1212@users.noreply.github.com>
2021-02-23 20:01:34 +0200

    Update test.java

--- java-pract-1-1-23-02/src/test/test.java
+++ java-pract-1-1-23-02/src/test/test.java
@@ -1,1 +1,21 @@
-1
+package test;
+
+import main.Contact;
+/**
+ *
+ * @author 38099
+ */
+public static void main(String[] args) {
+
+        Contact Boris = new Contact("Boris", "Ivanov", "Krasina 15");
+        Boris.Id = 1;
+        String BorisfirstName = Boris.getfirstName();
+        String BorislastName = Boris.getlastName();
+        String Borisaddress = Boris.getaddress();
+
+        System.out.println("ID: " + BorisfirstName);
+        System.out.println("First name: " + BorisfirstName);
+        System.out.println("Last name: " + BorislastName);
+        System.out.println("Address: " + Borisaddress);
+
+}
