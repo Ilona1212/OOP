091dc392cdedaa55def1f7f0ee4f3122451d12dc
Ilona1212 <59233009+Ilona1212@users.noreply.github.com>
2021-02-24 18:11:01 +0200

    Add files via upload

---
+++ java-pract-1-2-23-02/src/Test.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 38099
+ */
+public class Test {
+    public static void main(String[] args) {
+        Order order = new Order(1, "apple", "Vasya");
+        order.setDateTime("23 decmber 2021");
+        System.out.println(order);
+        Type type = type.RUSHORDER;
+        System.out.println(type);
+        
+    }
+    
+}
