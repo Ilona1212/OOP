b69befcc3763750e45ee0b47ce2688d1f0d8e905
Ilona1212 <59233009+Ilona1212@users.noreply.github.com>
2021-03-10 20:49:36 +0200

    Add files via upload

---
+++ java-pract-25-02/java-pract-1-03/src/Main/Contact.java
@@ -0,0 +1,33 @@
+package Main;
+
+public class Contact extends Students{
+    protected String Address; 
+
+    public Contact(String Address, int ID, String firstName, String lastName) {
+        super(ID, firstName, lastName);
+        this.Address = Address;
+    }
+    
+    public Contact( Students student, String Address )
+    {
+        super(student.getId(), student.getFirstName(), student.getLastName() );
+        this.Address = Address;
+    }
+    
+    public Contact(){};
+    
+    public String getAddress() {
+       return Address;
+   }
+    
+    public void setAddress( String _address )
+    {
+        this.Address = _address;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" + "Id=" + Id + ", FirstName=" + FirstName + ", LastName=" + LastName + ", Address=" + Address + '}';
+    }
+    
+}
